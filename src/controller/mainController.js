5a20d89c660170c21260939ee3c4bf37b6e14d34
ld0rn76 <87490792+ld0rn76@users.noreply.github.com>
2024-10-05 17:36:26 -0400

    added stock stuff

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -6,26 +6,38 @@ const home = async (req, res) => {
 
 const investing = async(req, res) => {
     console.log(process.env.STOCK_API_KEY)
-    let options = {
-        method: 'GET',
-        url: 'https://latest-stock-price.p.rapidapi.com/price',
-        params: {Indices: 'NIFTY 50'},
-        headers: {
-            'x-rapidapi-host': 'latest-stock-price.p.rapidapi.com',
-            'x-rapidapi-key': process.env.STOCK_API_KEY
-        }
-    };
-    
-    axios.request(options)
-    .then(response => {
-        const stocks = response.data;
-        console.log(stocks)
-        res.render("investing", stocks)
-    })
-    .catch(error => {
-        console.error(error);
-        res.render('error');
+    //apple, amazon, and walmart
+    const symbols = ["AAPL:NASDAQ", "WMT:NYSE", "AMZN:NASDAQ"]
+    let info = []
+    for(let symbol of symbols){
+        let options = {
+            method: 'GET',
+            url: 'https://real-time-finance-data.p.rapidapi.com/stock-quote',
+            params: {
+                symbol: symbol,
+                language: 'en'
+            },
+            headers: {
+            'x-rapidapi-key': process.env.STOCK_API_KEY,
+            'x-rapidapi-host': 'real-time-finance-data.p.rapidapi.com'
+            }
+        };
+
+        await axios.request(options)
+        .then(response => {
+            const stocks = response.data;
+            console.log(stocks)
+            info.push(stocks.data)
+            
         })
+        .catch(error => {
+            console.error(error);
+            res.render('error');
+        })
+    }
+        
+    console.log("TEST", info)
+    res.render("investing", {apple: info[0], walmart: info[1], amazon: info[2]})
 }
 
 module.exports = {home, investing}
