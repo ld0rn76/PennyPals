da13ecebdffd9994cf0949414a6d05366fc1e93f
Michael Forde <michael.kenneth.forde@gmail.com>
2024-10-05 16:02:51 -0400

    Setting up Auth0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DivHacking
+
+## Node Modules
+npm install express-openid-connect
+
+express-openid-connect creating using openssl rand -hex 32
+
+npm install dotenv
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { auth, requiresAuth } = require('express-openid-connect');
+const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'http://localhost:3000',
+  clientID: 'XZrjkLMoConQ2Er3CAdlQ4EYKgj7kLzW',
+  issuerBaseURL: 'https://dev-44wy1sttigf5kujn.us.auth0.com',
+  secret: 'LONG_RANDOM_STRING'
+};
+
+// The `auth` router attaches /login, /logout
+// and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.oidc.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+  )
+});
+
+// The /profile route will show the user profile as JSON
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
+});
